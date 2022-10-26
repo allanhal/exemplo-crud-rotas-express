1e4bd08aa5b10cfb99839ed5e94f9dc5b22de284
angelolustosa <>
2022-10-26 09:16:41 -0300

    safe check

--- controllers/comentario/comentario.js
+++ controllers/comentario/comentario.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
 function listar(produtoIdParam) {
-    return JSON.parse(fs.readFileSync(__dirname+'/comentario.json').toString())
-    .filter(({produtoId})=>produtoId === produtoIdParam);
+    return JSON.parse(fs.readFileSync(__dirname + '/comentario.json').toString())
+        .filter(({ produtoId }) => produtoId === produtoIdParam);
 }
 
-function listarAutor(produtoIdParam,autorParam) {
+function listarAutor(produtoIdParam, autorParam = '') {
     return listar(produtoIdParam)
-    .filter(({nomeDoAutor})=>nomeDoAutor.toLowerCase().indexOf(autorParam.toLowerCase()) >= 0)
+        .filter(({ nomeDoAutor }) => nomeDoAutor.toLowerCase().indexOf(autorParam.toLowerCase()) >= 0)
 }
-module.exports={
+module.exports = {
     listar,
     listarAutor
 }
--- controllers/comentario/router.js
+++ controllers/comentario/router.js
@@ -7,6 +7,11 @@ route.get('/comentario/:produtoId', (req, res) => {
     res.send(comentario.listar(req.params.produtoId))
 })
 route.get('/comentario', (req, res) => {
-    res.send(comentario.listarAutor(req.query.produtoId,req.query.autor))
+    if (!req.query.produtoId) {
+        res.status(400)
+        res.send("nao enviou o produtoID")
+    }
+    console.log('req.query.autor', req.query.autor)
+    res.send(comentario.listarAutor(req.query.produtoId, req.query.autor))
 })
 module.exports = route;
